c5f1e0b9642e72b137fe36dbfd17a58e98215bd7
WarmFreezer <thomas.eubank516@gmail.com>
2025-11-13 14:37:55 -0500

    Added chase and whiff percent to the chart.

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ path = os.path.abspath(os.path.dirname(__file__))
 
 for filename in os.listdir(path + '/app/input'):
     excel = pd.read_excel(path + '/app/input/' + filename)
-    table = excel[['Pitcher', 'PitcherId', 'TaggedPitchType', 'EffectiveVelo', 'InducedVertBreak', 'HorzBreak', 'SpinRate', 'VertApprAngle', 'HorzApprAngle', 'RelHeight', 'RelSide']] 
+    table = excel[['Pitcher', 'PitcherId', 'TaggedPitchType', 'RelSpeed', 'InducedVertBreak', 'HorzBreak', 'SpinRate', 'VertApprAngle', 'HorzApprAngle', 'RelHeight', 'RelSide', 'Extension', 'SpinAxis', 'ZoneTime', 'PlateLocHeight', 'PlateLocSide', 'PitchCall']] 
 
     for pitcherid in table['PitcherId'].unique():
         pitcher_data = table[table['PitcherId'] == pitcherid]
@@ -27,10 +27,13 @@ for filename in os.listdir(path + '/app/input'):
                        'Avg_Vaa': [],
                        'Avg_Haa': [],
                        'Avg_RelHeight': [],
-                       'Avg_RelSide': []}
-                        #Ext.,
-                        #Axis
-
+                       'Avg_RelSide': [],
+                       'Extension': [],
+                       'Axis': [],
+                       'Zone_Percent': [],
+                       'Chase_Percent': [],
+                       'Whiff_Percent': []}
+                        
         #Parse through each pitch type for the pitcher
         for pitch_type in pitcher_data['TaggedPitchType'].unique():
             pitch_type_data = pitcher_data[pitcher_data['TaggedPitchType'] == pitch_type]
@@ -40,7 +43,7 @@ for filename in os.listdir(path + '/app/input'):
                 game_report['PitchType'].append(pitch_type)
                 game_report['Count'].append(len(pitch_type_data))
                 game_report['PitchPercent'].append(len(pitch_type_data) / len(pitcher_data) * 100)
-                game_report['Avg_Velocity'].append(pitch_type_data['EffectiveVelo'].mean())
+                game_report['Avg_Velocity'].append(pitch_type_data['RelSpeed'].mean())
                 game_report['Avg_Ivb'].append(pitch_type_data['InducedVertBreak'].mean())
                 game_report['Avg_Hb'].append(pitch_type_data['HorzBreak'].mean())
                 game_report['Avg_SpinRate'].append(pitch_type_data['SpinRate'].mean())
@@ -48,6 +51,29 @@ for filename in os.listdir(path + '/app/input'):
                 game_report['Avg_Haa'].append(pitch_type_data['HorzApprAngle'].mean())
                 game_report['Avg_RelHeight'].append(pitch_type_data['RelHeight'].mean())
                 game_report['Avg_RelSide'].append(pitch_type_data['RelSide'].mean())
+                game_report['Extension'].append(pitch_type_data['Extension'].mean())
+                game_report['Axis'].append(pitch_type_data['SpinAxis'].mean()) 
+                game_report['Zone_Percent'].append(pitch_type_data['ZoneTime'].mean() * 100)
+
+                chase_count = 0
+                for _, row in pitch_type_data.iterrows():
+                    outside = (row['PlateLocHeight'] < 1.5) or (row['PlateLocHeight'] > 3.5)
+                    outside_height = abs(row['PlateLocSide']) > 0.708
+                    batter_swung = row['PitchCall'] in ['StrikeSwinging', 'FoulBallNotFieldable', 'InPlay', 'HitByPitch']
+                    
+                    if (outside or outside_height) and batter_swung:
+                        chase_count += 1
+                        
+                game_report['Chase_Percent'].append(chase_count / len(pitch_type_data) * 100.00)
+                
+                whiff_count = 0
+                for _, row in pitch_type_data.iterrows():
+                    swing_and_miss = row['PitchCall'] in ['StrikeSwinging', 'FoulBallNotFieldable']
+                    
+                    if swing_and_miss:
+                        whiff_count += 1
+                
+                game_report['Whiff_Percent'].append(whiff_count / len(pitch_type_data) * 100.00)
         
         #Build and print report dataframe
         report_df = pd.DataFrame(game_report)
