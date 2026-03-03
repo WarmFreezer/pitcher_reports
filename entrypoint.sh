9c124facde74a795b371a57428ba91891529d725
Thomas Eubank <thomas.eubank516@gmail.com>
2026-03-03 17:35:59 -0500

    Fix path in entrypoint script for seeding storage volume

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 echo "Seeding volume..."
 
 # For each file baked into the image, copy to volume if not already there
-find /app/app/storage/defaults -type f | while read src; do
+find /app/storage/defaults -type f | while read src; do
   # Replace /defaults/ with the actual storage path
   dest=$(echo "$src" | sed 's|/defaults||')
   if [ ! -f "$dest" ]; then
